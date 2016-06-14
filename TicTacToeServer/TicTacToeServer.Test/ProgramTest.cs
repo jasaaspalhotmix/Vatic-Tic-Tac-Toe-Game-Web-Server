bc05f51e1d3ef64d032db861630f2f4cfb26f8b2
Shawn Horvatic <SH50169@pekininsurance.com>
2016-06-14 14:10:43 -0500

    Use NuGet package now:

--- TicTacToeServer/TicTacToeServer.Test/ProgramTest.cs
+++ TicTacToeServer/TicTacToeServer.Test/ProgramTest.cs
@@ -19,32 +19,11 @@ namespace TicTacToeServer.Test
             correctOutput.Append("Invaild Port Detected.");
             correctOutput.Append("Vaild Ports 2000 - 65000");
 
-            string[] argsHelloWorld = {"-p", invaildPorts.ToString()};
+            string[] argsTicTacToe = {"-p", invaildPorts.ToString()};
             var mockPrinterOne = new MockPrinter();
-            var serverMadeHelloWorld = Program.MakeServer(argsHelloWorld, mockPrinterOne);
-            Assert.Null(serverMadeHelloWorld);
+            var serverMadeTicTacToe = Program.MakeServer(argsTicTacToe, mockPrinterOne);
+            Assert.Null(serverMadeTicTacToe);
             mockPrinterOne.VerifyPrint(correctOutput.ToString());
-
-            string[] args = {"-p", invaildPorts.ToString(), "-d", "C:\\"};
-            var mockPrinterTwo = new MockPrinter();
-            var serverMade = Program.MakeServer(args, mockPrinterTwo);
-            Assert.Null(serverMade);
-            mockPrinterTwo.VerifyPrint(correctOutput.ToString());
-
-            var mockPrinterThree = new MockPrinter();
-            string[] argsSwaped = {"-d", "C:\\", "-p", invaildPorts.ToString()};
-            var serverMadeSwaped = Program.MakeServer(argsSwaped, mockPrinterThree);
-            Assert.Null(serverMadeSwaped);
-            mockPrinterThree.VerifyPrint(correctOutput.ToString());
-        }
-
-        [Fact]
-        public void Make_Dirctory_Server_Correct()
-        {
-            var mockPrinter = new MockPrinter();
-            string[] args = {"-p", "32000", "-d", "C:\\"};
-            var serverMade = Program.MakeServer(args, mockPrinter);
-            Assert.NotNull(serverMade);
         }
 
         [Fact]
@@ -53,7 +32,7 @@ namespace TicTacToeServer.Test
             var mockPrinter = new MockPrinter();
             var correctOutput = new StringBuilder();
 
-            string[] args = {"-p", "8765", "-d", "C:\\"};
+            string[] args = {"-p", "8765"};
             var serverMade = Program.MakeServer(args, mockPrinter);
             Assert.NotNull(serverMade);
 
@@ -62,27 +41,6 @@ namespace TicTacToeServer.Test
             mockPrinter.VerifyPrint("Another Server is running on that port");
         }
 
-        [Fact]
-        public void Make_Dirctory_Server_Correct_Arg_Backwords()
-        {
-            var mockPrinter = new MockPrinter();
-            string[] args = {"-d", "C:\\", "-p", "2020"};
-            var serverMade = Program.MakeServer(args, mockPrinter);
-            Assert.NotNull(serverMade);
-        }
-
-        [Fact]
-        public void Make_Dirctory_Server_Inncorect_Correct_Not_Dir()
-        {
-            var mockPrinter = new MockPrinter();
-            var correctOutput = new StringBuilder();
-
-            string[] args = {"-d", "Hello", "-p", "3258"};
-            var serverMade = Program.MakeServer(args, mockPrinter);
-            Assert.Null(serverMade);
-
-            mockPrinter.VerifyPrint("Not a vaild directory");
-        }
 
         [Fact]
         public void Make_Dirctory_Server_Inncorect_Correct_Not_Port()
@@ -92,7 +50,7 @@ namespace TicTacToeServer.Test
             correctOutput.Append("Invaild Port Detected.");
             correctOutput.Append("Vaild Ports 2000 - 65000");
 
-            string[] args = {"-d", "C:\\", "-p", "hello"};
+            string[] args = {"-p", "hello"};
             var serverMade = Program.MakeServer(args, mockPrinter);
             Assert.Null(serverMade);
             mockPrinter.VerifyPrint(correctOutput.ToString());
@@ -100,21 +58,7 @@ namespace TicTacToeServer.Test
 
 
         [Fact]
-        public void Make_Dirctory_Server_Inncorect_Correct()
-        {
-            var mockPrinter = new MockPrinter();
-            var correctOutput = new StringBuilder();
-
-            string[] args = {"-p", "32000", "-d", "-d"};
-            var serverMade = Program.MakeServer(args, mockPrinter);
-            Assert.Null(serverMade);
-
-            mockPrinter.VerifyPrint("Not a vaild directory");
-        }
-
-
-        [Fact]
-        public void Make_Hello_World_Server_Correct()
+        public void Make_TicTacToe_Server_Correct()
         {
             var mockPrinter = new MockPrinter();
             string[] args = {"-p", "9560"};
@@ -123,7 +67,7 @@ namespace TicTacToeServer.Test
         }
 
         [Fact]
-        public void Make_Hello_World_Incorrect_Correct()
+        public void Make_TicTacToe_Incorrect_Correct()
         {
             var mockPrinter = new MockPrinter();
             string[] args = {"2750", "-p"};
@@ -132,7 +76,7 @@ namespace TicTacToeServer.Test
         }
 
         [Fact]
-        public void Make_Hello_World_Incorrect_Correct_No_Port()
+        public void Make_TicTacToe_Incorrect_Correct_No_Port()
         {
             var mockPrinter = new MockPrinter();
             string[] args = {"-p", "-p"};
@@ -147,11 +91,9 @@ namespace TicTacToeServer.Test
             var correctOutput = new StringBuilder();
             correctOutput.Append("Invaild Number of Arguments.\n");
             correctOutput.Append("Can only be -p PORT\n");
-            correctOutput.Append("or -p PORT -d DIRECTORY\n");
             correctOutput.Append("Examples:\n");
-            correctOutput.Append("Server.exe -p 8080 -d C:/\n");
-            correctOutput.Append("Server.exe -d C:/HelloWorld -p 5555\n");
-            correctOutput.Append("Server.exe -p 9999");
+            correctOutput.Append("Server.exe -p 8080\n");
+           correctOutput.Append("Server.exe -p 9999");
 
             var args = new[] {"-s"};
             var serverMade = Program.MakeServer(args, mockPrinter);
@@ -175,15 +117,5 @@ namespace TicTacToeServer.Test
             mockServer.VerifyAcceptingNewConn();
         }
 
-        [Fact]
-        public void Make_Log_File()
-        {
-            var mockPrinter = new MockPrinter();
-
-            string[] args = {"-p", "10560", "-l", "c:/TestLog"};
-            var serverMade = Program.MakeServer(args, mockPrinter);
-            Assert.Equal("c:/TestLog", mockPrinter.Log);
-            Assert.NotNull(serverMade);
-        }
     }
 }
---
+++ TicTacToeServer/TicTacToeServer.Test/Service404Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Server.Core;
+using TicTacToeServer.Core;
+using Xunit;
+
+namespace TicTacToeServer.Test
+{
+    public class Service404Test
+    {
+        [Fact]
+        public void Make_New_Class_Not_Null()
+        {
+            Assert.NotNull(new Service404());
+        }
+
+        [Fact]
+        public void Cant_Process_Anything()
+        {
+            var service404 = new Service404();
+            Assert.False(service404.CanProcessRequest("", null));
+        }
+
+        [Fact]
+        public void Error_Message()
+        {
+            var correctOutput = new StringBuilder();
+            correctOutput.Append(@"<!DOCTYPE html>");
+            correctOutput.Append(@"<html>");
+            correctOutput.Append(@"<head><title>Vatic Server 404 Error Page</title></head>");
+            correctOutput.Append(@"<body>");
+            correctOutput.Append(@"<h1>404, Can not process request on port 5555</h1>");
+            correctOutput.Append(@"</body>");
+            correctOutput.Append(@"</html>");
+            var httpPackage = new HttpResponse();
+            var serverProperties = new ServerProperties(null,
+                5555, new HttpResponse(), new ServerTime(),
+                new MockPrinter());
+            var service404 = new Service404();
+            httpPackage = (HttpResponse)service404.ProcessRequest("", httpPackage, serverProperties);
+            Assert.Equal("404 Not Found", httpPackage.HttpStatusCode);
+            Assert.Equal(correctOutput.ToString(), httpPackage.Body);
+        }
+    }
+}
